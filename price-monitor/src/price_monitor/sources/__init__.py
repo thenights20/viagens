1e2c2316ef7c3d4b99b82e2f54165119df928f60
the_nights <61749786+thenights20@users.noreply.github.com>
2026-09-10 12:27:23 -0400

    feat: registrar varejistas diretos

--- price-monitor/src/price_monitor/sources/__init__.py
+++ price-monitor/src/price_monitor/sources/__init__.py
@@ -1,5 +1,6 @@
 from .casasbahia import CasasBahiaSource
 from .mercadolivre import MercadoLivreSource
+from .retailer import RetailerSource
 from .shopee import ShopeeSource
 
-__all__ = ["CasasBahiaSource", "MercadoLivreSource", "ShopeeSource"]
+__all__ = ["CasasBahiaSource", "MercadoLivreSource", "RetailerSource", "ShopeeSource"]
